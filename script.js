97ec24d40ff379779e0475cedb19acedbe3ca157
raquel26mg <raquel.matgonz@gmail.com>
2025-03-30 19:11:03 +0200

    C3: Varios cambios

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const cards = [
-    { question: "Colores de la bandera de Francia", answer: ["Azul", "Blanco", "Rojo"], colors: ["#0055A4", "#FFFFFF", "#EF4135"], number: 3 },
-    { question: "Colores de la bandera de Italia", answer: ["Verde", "Blanco", "Rojo"], colors: ["#009246", "#FFFFFF", "#CE2B37"], number: 3 },
-    { question: "Color del cielo durante el día", answer: ["Azul"], colors: ["#1E90FF"], number: 1 },
-    { question: "Colores de una sandía por dentro y por fuera", answer: ["Rojo", "Verde"], colors: ["#FC3D32", "#4CAF50"], number: 2 }
+    { question: "Colores de la bandera de Francia", answer: ["Azul", "Blanco", "Rojo"], colors: ["#0055A4", "#FFFFFF", "#EF4135"], number: 3, details: "" },
+    { question: "Colores de la bandera de Italia", answer: ["Verde", "Blanco", "Rojo"], colors: ["#009246", "#FFFFFF", "#CE2B37"], number: 3, details: "" },
+    { question: "Color del cielo durante el día", answer: ["Azul"], colors: ["#1E90FF"], number: 1, details: "" },
+    { question: "Colores de una sandía por dentro y por fuera", answer: ["Rojo", "Verde"], colors: ["#FC3D32", "#4CAF50"], number: 2, details: "" },
+    { question: "Las Supernenas", answer: ["Azul", "Rosa", "Verde"], colors: ["#0055A4", "#FF4E88", "#4CAF50"], number: 3, details: "Burbuja, Pétalo y Cactus" }
 ];
 
 let remainingCards = [...cards];
@@ -35,8 +36,16 @@ function newCard() {
     const colorsText = document.createElement("p");
     colorsText.textContent = currentCard.answer.join(", ");
     colorsText.classList.add("colors-text");
-    
-    answerContainer.append(colorsContainer, colorsText);
+
+    if(currentCard.details != "") {
+        const detailsText = document.createElement("p");
+        detailsText.textContent = currentCard.details;
+        detailsText.classList.add("details-text");
+
+        answerContainer.append(colorsContainer, colorsText, detailsText);
+    } else {
+        answerContainer.append(colorsContainer, colorsText);
+    }
     
     document.querySelector(".card").classList.remove("flipped");
     flipped = false;
